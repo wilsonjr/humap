265f8eba9a2de02809a9351a61c179f237279d27
Wilson <wilson_jr@outlook.com>
2021-10-26 11:20:49 -0300

    Add HUMAP and UMAP (C++) tests

--- tests/test_humap.py
+++ tests/test_humap.py
@@ -11,7 +11,9 @@ from sklearn.model_selection import train_test_split
 class TestUmap(unittest.TestCase):
 
     def setUp(self):
-        X, y = fetch_openml('mnist_784', version=1, return_X_y=True)        
+        X, y = fetch_openml('mnist_784', version=1, return_X_y=True)    
+        _, X, _, y = train_test_split(X, y, test_size=0.2, random_state=0)
+
         X = normalize(X)
 
         self.X = X 
@@ -32,7 +34,8 @@ class TestUmap(unittest.TestCase):
         influence2to1 = reducer.influence(2)
         influence1to0 = reducer.influence(1)
 
-        self.assertEqual((level1.shape[1], level0.shape[1]), (np.sum(influence2to1), np.sum(influence1to0)), "missing influence on hierarchy levels")
+        self.assertEqual(np.sum(influence2to1), level0.shape[0], "missing influence on hierarchy levels")
+        self.assertEqual(np.sum(influence2to1), np.sum(influence1to0), "missing influence on hierarchy levels")
 
     def test_lessPointsOnTop(self):
 
@@ -44,20 +47,20 @@ class TestUmap(unittest.TestCase):
         level1 = reducer.transform(1)
         level0 = reducer.transform(0)
 
-        self.assertLess(level1.shape[1], level2.shape[2])
-        self.assertLess(level1.shape[0], level2.shape[1])
+        self.assertLess(level2.shape[0], level1.shape[0])
+        self.assertLess(level1.shape[0], level0.shape[0])
 
     def test_dimensionality2(self):
 
         X = np.random.rand(1000, 1)
         reducer = humap.HUMAP(n_neighbors=15)
 
-        self.assertRaises(ValueError, lambda: reducer.fit(X), "input dimensionality <= 2")
+        self.assertRaises(ValueError, reducer.fit, X)
 
     def test_noneArray(self):
         reducer = humap.HUMAP(n_neighbors=15)
 
-        self.assertRaises(ValueError, lambda: reducer.fit(None), "input array must be a valid array")
+        self.assertRaises(ValueError, reducer.fit, None)
 
 
     def test_shapeArray(self):
@@ -65,21 +68,21 @@ class TestUmap(unittest.TestCase):
         X = np.random.rand(1000, 2, 3)
         reducer = humap.HUMAP(n_neighbors=15)
 
-        self.assertRaises(ValueError, lambda: reducer.fit(X), "input array must be two-dimensional")
+        self.assertRaises(ValueError, reducer.fit, X)
 
     def test_nNeighborsGreaterThanLevel(self):
 
         X = np.random.rand(1000, 2)
         reducer = humap.HUMAP(n_neighbors=100)
 
-        self.assertRaises(ValueError, lambda: reducer.fit(X), "input array must be two-dimensional")
+        self.assertRaises(ValueError, reducer.fit, X)
 
     def test_ndArray(self):
 
         X = np.random.rand(1000, 2).tolist()
         reducer = humap.HUMAP(n_neighbors=10)
 
-        self.assertRaises(ValueError, lambda: reducer.fit(X), "input array must be two-dimensional")
+        self.assertRaises(ValueError, reducer.fit, X, "input array must be two-dimensional")
 
 
 
